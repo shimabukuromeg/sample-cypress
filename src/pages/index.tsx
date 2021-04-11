c432819b58be0f8893c567c4eb68fabe8430bee6
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2021-04-11 10:53:29 +0900

    [update] ディレクトリ構成変更、不要ファイル削除

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div>
+        <p>Hello, World.</p>
+      </div>
+    </div>
+  )
+}
